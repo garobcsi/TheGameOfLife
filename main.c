541b75b0d94c64a00e75e7025f98572f30ddcea0
garobcsi <gasparrobert0908@gmail.com>
2023-10-23 22:23:56 +0200

    Added Inits To main.c

--- main.c
+++ main.c
@@ -1,7 +1,12 @@
-#include "src/terminal/PrintHandler.h"
+#include "src/game/FileHandler.h"
+#include "src/game/Random.h"
+#include "src/terminal/GameSession.h"
 #include "debugmalloc.h"
 
 int main() {
+    InitSaveFolder();
+    InitRandom();
+    StartTerminalSession();
 
     return 0;
 }
