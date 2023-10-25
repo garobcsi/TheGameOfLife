0b1dd4c8cc7cbcd0d98391d840272734018eae98
garobcsi <gasparrobert0908@gmail.com>
2023-10-25 09:08:10 +0200

    Get Window Size in Main.c

--- main.c
+++ main.c
@@ -2,17 +2,23 @@
 #include "src/game/Random.h"
 #include "src/terminal/GameSession.h"
 #include "src/terminal/PrintHandler.h"
+#include "src/terminal/WindowSize.h"
 #include "debugmalloc.h"
 
 int main() {
     InitSaveFolder();
     InitRandom();
-    
+
     #ifdef _WIN32
     InitPrintUTF8ForWindows();
     #endif
 
     ClearScr();
 
+    WindowSize * winSize = GetWindowSize();
+    StartTerminalSession(winSize);
+
+
+    DestroyWindowSize(winSize);
     return 0;
 }
