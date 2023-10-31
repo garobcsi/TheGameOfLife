15261ecee7614a539be9665c23119f432a6bfb27
garobcsi <gasparrobert0908@gmail.com>
2023-10-31 22:10:02 +0100

    Made File Handler's struct and define's

--- src/game/FileHandler.h
+++ src/game/FileHandler.h
@@ -1,3 +1,17 @@
-#define SAVE_FOLDER saves
+#include <stdbool.h>
+
+#define SAVE_FOLDER saves // folder name
+#define FILE_FORMAT csv // .csv files
+#define FILE_NAME_LENGTH 128
+
+#ifndef FILE_HANDLER
+#define FILE_HANDLER
+
+typedef struct LoadedFileProperties {
+    char fileName[FILE_NAME_LENGTH];
+    bool didUserSave;
+}LoadedFileProperties;
+
+#endif /* FILE_HANDLER */
 
 void InitSaveFolder();
