cd20afd8ad0c7805fd7e36596c31f7e093d67211
garobcsi <gasparrobert0908@gmail.com>
2023-11-01 21:02:37 +0100

    Made Menu handling

--- src/terminal/GameSession.c
+++ src/terminal/GameSession.c
@@ -1,8 +1,68 @@
+#include "GameSession.h"
 #include "PrintHandler.h"
 #include "PromptHandler.h"
+#include "../../debugmalloc.h"
+#include <stdio.h>
 
-void StartTerminalSession(WinSize winSize) {
-    PrintLogo(winSize);
+int LoadMenu(MenuOption menuOption,Game * game){
+    switch (menuOption) {
+        case mainMenu:
+            HandleMainMenu(game);
+            break;
+        case newGame:
+            HandleNewGame(game);
+            break;
+        case loadGame:
+            HandleLoadGame(game);
+            break;
+        case mainGame:
+            HandleMainGame(game);
+            break;
+        case quitGame:
+            HandleQuit(game);
+            break;
+        case abortGame:
+            return 1;
+            break;
+        default:
+            return 0;
+            break;
+    }
+    return 0;
+}
+
+void HandleMainMenu(Game * game) {
+    PrintLogo(game->winSize);
     PrintMainMenu();
-    PromptMainMenu();
+    int select = PromptMainMenu();
+    switch (select) {
+        case 1:
+            LoadMenu(newGame,game);
+            break;
+        case 2:
+            LoadMenu(loadGame,game);
+            break;
+        default:
+            return;
+            break;
+    }
+}
+void HandleNewGame(Game * game) {
+    PrintNewGameTitle();
+    char * str = PromptFileName();
+    if(str == NULL) {
+        LoadMenu(abortGame,game);
+        return;
+    }
+    printf("\n\n%s",str);
+    free(str);
+}
+void HandleLoadGame(Game * game) {
+
+}
+void HandleMainGame(Game * game) {
+
+}
+void HandleQuit(Game * game) {
+
 }
--- src/terminal/GameSession.h
+++ src/terminal/GameSession.h
@@ -1,5 +1,18 @@
 #define X 20
 #define Y 20
 #include "WindowSize.h"
+#include "../game/GameLogic.h"
 
-void StartTerminalSession(WinSize winSize);
+#ifndef GAME_SESSION
+#define GAME_SESSION
+
+typedef enum { mainMenu,newGame,loadGame,mainGame,quitGame,abortGame } MenuOption;
+
+#endif /* GAME_SESSION */
+
+int  LoadMenu(MenuOption menuOption,Game * game);
+void HandleMainMenu(Game * game);
+void HandleNewGame(Game * game);
+void HandleLoadGame(Game * game);
+void HandleMainGame(Game * game);
+void HandleQuit(Game * game);
