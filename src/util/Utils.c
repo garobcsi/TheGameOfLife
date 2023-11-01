1f00b5d6b4985b00e88deaf5d763bb2936ef73af
garobcsi <gasparrobert0908@gmail.com>
2023-11-01 20:36:41 +0100

    Remove Util exit bc it wont free allocated memory

--- src/util/Utils.c
+++ src/util/Utils.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include "../terminal/StyleHandler.h"
 
 void AbortMsg(char str[]) {
     AnsiColorRed();
     printf("\n%s\n",str);
     AnsiResetAll();
-    exit(1);
 }
