a5e0fae7b8a475bafb62705b2be25bda4071d6ee
garobcsi <gasparrobert0908@gmail.com>
2023-11-24 17:51:23 +0100

    removed fclose on NULL

--- src/game/FileHandler.c
+++ src/game/FileHandler.c
@@ -91,7 +91,7 @@ int SaveMatrixToFile(Matrix * matrix,char * str) {
     char * path = MakePath(str,true);
     file = fopen(path, "w");
     if (file == NULL) {
-        fclose(file);
+        //fclose(file);
         AbortMsg("Failed to create file!");
         return 1;
     }
@@ -117,7 +117,7 @@ int GetSizeFromFile (SizeMatrix * size, GameSaveFiles * files,int select) {
 
     FILE *file = fopen(path, "r");
     if (file == NULL) {
-        fclose(file);
+        //fclose(file);
         AbortMsg("Error opening file");
         return 1;
     }
@@ -149,7 +149,7 @@ int LoadGameFromFile(Game * game,GameSaveFiles * files,int select) {
 
     FILE *file = fopen(path, "r");
     if (file == NULL) {
-        fclose(file);
+        //fclose(file);
         AbortMsg("Error opening file");
         return 1;
     }
